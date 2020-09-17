f84d7a4536a2a6ce1da9a4043d7c96bb66a4a7c1
nelfy84321 <61667659+nelfy84321@users.noreply.github.com>
2020-09-17 15:03:53 +0300

    deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAPuOW0p8LvHHLqtwxWV-SO8UWatvoeWBA",
+    authDomain: "inventor-fifth-hw.firebaseapp.com",
+    databaseURL: "https://inventor-fifth-hw.firebaseio.com",
+    projectId: "inventor-fifth-hw",
+    storageBucket: "inventor-fifth-hw.appspot.com",
+    messagingSenderId: "677692002788",
+    appId: "1:677692002788:web:9e5dbc487c6e083f74b7f1",
+    measurementId: "G-65J64N06P7"
+  }
 };
