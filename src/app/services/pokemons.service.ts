750541023a3c4d2e9be4f8ddd8784ee7371aeb9a
nelfy84321 <61667659+nelfy84321@users.noreply.github.com>
2020-09-17 15:48:20 +0300

    Update pokemons.service.ts

--- src/app/services/pokemons.service.ts
+++ src/app/services/pokemons.service.ts
@@ -1,8 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
-// import { PokemonsComponent } from '../pages/pokemons/pokemons.component';
-// import { PokemonsComponent } from './../pages/pokemons/pokemons.component';
+
 
 
 export interface Item {
